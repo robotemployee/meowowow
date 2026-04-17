afae7fec0e139d75d3eba84b7806120f71861965
nimeemaw <mediocrealarm72@gmail.com>
2026-04-16 17:19:59 -0700

    meiaow

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import time
 
 from XRPLib.defaults import *
+import colorsys
+from typing import Tuple, cast
 
 board: Board = Board.get_default_board()
-gamepad: Gamepad = Gamepad.get_default_gamepad()
+
+hue: float = 0
 
 while True:
     board.set_rgb_led(255,255,0)
-    
-    time.sleep(0.05)
+    outputRgb: tuple[float, float, float] = cast(tuple[float, float, float], colorsys.hsv_to_rgb(hue, 1, 1))
+    hue += 0.01
+    if (hue > 1): hue = 0
+    time.sleep(0.01)
+
